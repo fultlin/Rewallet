87ca66889d9f3f84ec01a0f777ea7f0dd46b7ef1
fultlin <ugolok.kita@gmail.com>
2023-12-19 00:17:48 +0300

    a11y_last

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-let burgerList = document.querySelector('.burger__list');
-let burger = document.querySelector('.burger')
+let burgerList = document.querySelector(".burger__list");
+let burger = document.querySelector(".burger");
 
 burger.onclick = function () {
-	burgerList.classList.toggle('active');
-}
-burgerList.classList.toggle('active');
+  burgerList.classList.toggle("active");
+};
+burgerList.classList.toggle("active");
+
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1;
+  } else if (4 <= month <= 6) {
+    return 2;
+  } else if (month <= 9) {
+    return 3;
+  } else if (month <= 12) {
+    return 4;
+  }
+};
+
